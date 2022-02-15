474a34a28e3fd80a235274ebbd0e77170d96aeb9
Houle Adrien <46323356+biscuitvanille@users.noreply.github.com>
2022-02-15 11:07:22 +0100

    modif graphique

--- dist/index.js
+++ dist/index.js
@@ -52,7 +52,7 @@ Array(200).fill().forEach(addStar);
 
 // Background
 
-const spaceTexture = new THREE.TextureLoader().load('space.gif');
+const spaceTexture = new THREE.TextureLoader().load('space.jpg');
 scene.background = spaceTexture;
 
 // Logo
